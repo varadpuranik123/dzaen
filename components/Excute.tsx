1801e3b318ab2470af01cc30993d0f975c2331c9
vdpop <varadpuranik123@gmail.com>
2024-04-03 00:52:09 +0530

    created execution section

--- components/Excute.tsx
+++ components/Excute.tsx
@@ -2,8 +2,67 @@ import React from 'react'
 
 const Excute = () => {
   return (
-    <div className="w-full h-[150vh] " >
-        Excute
+    <div className="flex flex-col w-full h-[190vh] " >
+        <div className="mt-28 h-[8%] w-full text-center space-y-1">
+            <p className=" text-[#333] text-6xl ">This is the Way we Execute <span className="text-[#FF995F] font-bold ">Boss</span></p>
+            <p className=" text-[#333]/60 font-medium text-lg ">Efficiency, Precision, Excellence: Our Signature Approach</p>
+        </div>
+        <div className="flex flex-col w-full h-[92%] mt-10  ">
+            <div className="flex w-full h-full border-t border-[#FF995F] ">
+                <div className="flex flex-col py-16 px-20 space-y-4 w-full h-full ">
+                    <div className=" flex items-center w-full h-[20%] border-l-[4px] rounded-lg bg-gradient-to-r from-[#FF995F]/20 pl-4 border-[#FF995F] ">
+                        <p className="text-4xl font-[500] ">Initial Consultation</p>
+                    </div>
+                    <div className="w-full h-[80%] ">
+                        <p className=" text-2xl text-[#333]/60 font-[400] ">We start with a detailed discussion to understand your goals, preferences, and target audience. We gather insights into your brand identity, desired aesthetics, and functionality requirements.</p>
+                    </div>
+                </div>
+                <div className="flex flex-col py-16 px-20 space-y-4 w-full h-full border-l border-[#FF995F] ">
+                    <div className=" flex items-center w-full h-[20%] border-l-[4px] rounded-lg bg-gradient-to-r from-[#FF995F]/20 pl-4 border-[#FF995F] ">
+                        <p className="text-4xl font-[500] ">Conceptualization and Planning</p>
+                    </div>
+                    <div className="w-full h-[80%] ">
+                        <p className=" text-2xl text-[#333]/60 font-[400] ">Our team brainstorms ideas and develops a strategic plan tailored to your objectives. We create wireframes and prototypes to visualize the layout and flow of your landing page.</p>
+                    </div>
+                </div>
+            </div>
+            <div className="flex w-full h-full border-t border-[#FF995F]  ">
+            <div className="flex flex-col py-16 px-20 space-y-4 w-full h-full ">
+                <div className=" flex items-center w-full h-[20%] border-l-[4px] rounded-lg bg-gradient-to-r from-[#FF995F]/20 pl-4 border-[#FF995F] ">
+                        <p className="text-4xl font-[500] ">Design and Development</p>
+                    </div>
+                    <div className="w-full h-[80%] ">
+                        <p className=" text-2xl text-[#333]/60 font-[400] ">Once the plan is approved, we proceed to the design phase. Our designers craft a custom layout, incorporating your brand elements and design preferences. Simultaneously, our developers bring the design to life, ensuring seamless functionality and responsiveness across devices.</p>
+                    </div>
+                </div>
+                <div className="flex flex-col py-16 px-20 space-y-4 w-full h-full border-l border-[#FF995F] ">
+                    <div className=" flex items-center w-full h-[20%] border-l-[4px] rounded-lg bg-gradient-to-r from-[#FF995F]/20 pl-4 border-[#FF995F] ">
+                        <p className="text-4xl font-[500] ">Feedback and Revisions</p>
+                    </div>
+                    <div className="w-full h-[80%] ">
+                        <p className=" text-2xl text-[#333]/60 font-[400] ">We present the initial design for your feedback and input. Based on your suggestions, we make revisions to refine the design until it meets your expectations.</p>
+                    </div>
+                </div>
+            </div>
+            <div className="flex w-full h-full border-t border-b border-[#FF995F]  ">
+            <div className="flex flex-col py-16 px-20 space-y-4 w-full h-full ">
+                <div className=" flex items-center w-full h-[20%] border-l-[4px] rounded-lg bg-gradient-to-r from-[#FF995F]/20 pl-4 border-[#FF995F] ">
+                        <p className="text-4xl font-[500] ">Delivery and Launch</p>
+                    </div>
+                    <div className="w-full h-[80%] ">
+                        <p className=" text-2xl text-[#333]/60 font-[400] ">Once everything is polished and perfected, we deliver the final product to you. We assist with the deployment process and provide support during the launch phase to ensure a smooth transition.</p>
+                    </div>
+                </div>
+                <div className="flex flex-col py-16 px-20 space-y-4 w-full h-full border-l border-[#FF995F] ">
+                    <div className=" flex items-center w-full h-[20%] border-l-[4px] rounded-lg bg-gradient-to-r from-[#FF995F]/20 pl-4 border-[#FF995F] ">
+                        <p className="text-4xl font-[500] ">Post-launch Support</p>
+                    </div>
+                    <div className="w-full h-[80%] ">
+                        <p className=" text-2xl text-[#333]/60 font-[400] ">Our commitment doesn't end with the launch. We offer ongoing support and maintenance to address any issues and ensure your landing page continues to perform at its best.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
   )
 }
